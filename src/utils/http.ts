d8a5166cb47da087f6b9e6c09c0053c8b128415d
fangkai <1209683835@qq.com>
2022-05-26 14:37:54 +0800

    feat: 商品

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,6 @@
 import qs from "qs";
 
-const mmsUrl = process.env.VUE_APP_MMS_URL;
-const liveUrl = process.env.VUE_APP_LIVE_URL;
+const apiUrl = process.env.VUE_APP_API_URL;
 
 interface Config extends RequestInit {
   data?: object;
@@ -11,32 +10,104 @@ export const http = async (
   endpoint: string,
   { data, headers, ...customConfig }: Config = {}
 ) => {
+  const token = localStorage.getItem("token") || "";
   const config = {
     method: "GET",
     headers: {
-      version: "2.1.2",
-      token: localStorage.getItem("token") || "",
-      "Content-Type": data ? "application/x-www-form-urlencoded" : "",
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+      timestamp: initTimestamp(),
+      nonce: initNonce(),
       ...headers,
     },
     ...customConfig,
   };
 
   if (config.method.toUpperCase() === "GET") {
-    if (endpoint.includes("?r=")) {
-      endpoint += `${qs.stringify(data)}`;
-    } else {
-      endpoint += `?${qs.stringify(data)}`;
-    }
+    endpoint += `?${qs.stringify(data)}`;
   } else {
     config.body = qs.stringify(data || {});
   }
 
-  return window
-    .fetch(`${endpoint.includes("?r=") ? liveUrl : mmsUrl}${endpoint}`, config)
-    .then(async (response) => {
-      const data = await response.json();
-      if (response.ok && data.code === 1001) return data.data;
-      else return Promise.reject(data);
-    });
+  return window.fetch(`${apiUrl}${endpoint}`, config).then(async (response) => {
+    const result = await response.json();
+    if ([200, 201, 204].includes(result.code)) return result.data;
+    else return Promise.reject(result);
+  });
+};
+
+export const initTimestamp = () =>
+  Math.round(new Date().getTime() / 1000).toString();
+
+export const initNonce = () => {
+  const chars = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let nonce = "";
+  for (let i = 0; i < 16; i++) {
+    const id = parseInt(`${Math.random() * 61}`);
+    nonce += chars[id];
+  }
+  return nonce;
 };
